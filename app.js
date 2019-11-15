623d1f3a4e2bb11f0e5e7ab6a56d2f473bf74fb1
William Strew <roquestrew@Williams-MacBook-Pro.local>
2019-11-14 20:49:50 -0600

    Instantiating Sonny

--- app.js
+++ app.js
@@ -13,3 +13,7 @@ class Person {
     console.log('Email: ' + this.email + ', Phone: ' + this.phone + '!');
   }
 }
+
+// Instantiate an instance object of Person with name of 'Sonny', email of 'sonny@hotmail.com', and phone of '483-485-4948', store it in the variable sonny.
+
+const sonny = new Person('Sonny', 'sonny@hotmail.com', '483-485-4948');
